f9369975afccbdd8dbaef159aee455900da7960c
pkeen <pkeen7@gmail.com>
2025-08-28 12:05:03 +0100

    switch to inner deps

--- apps/thia-demo-app/next.config.ts
+++ apps/thia-demo-app/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+
+	// Note: this silences a warning about which pnpm-lock is being used
+	turbopack: {
+		root: path.resolve(__dirname, "..", ".."),
+	},
 };
 
 export default nextConfig;
