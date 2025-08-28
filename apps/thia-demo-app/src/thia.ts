5c1aed1827044b2a935bd1bfaf1053241b366716
pkeen <pkeen7@gmail.com>
2025-08-28 10:43:11 +0100

    restore outer dependency management

--- apps/thia-demo-app/src/thia.ts
+++ apps/thia-demo-app/src/thia.ts
@@ -1,1 +1,23 @@
 import { createAuthManager } from "@pete_keen/thia-n-core";
+import { DrizzleAdapter } from "@pete_keen/thia-n-core";
+
+// export const thia = createAuthManager({
+// 	strategy: "jwt",
+// 	jwtConfig: {
+// 		access: {
+// 			name: "access", // for now the names NEED to be access and refresh
+// 			secretKey: "asfjsdkfj",
+// 			algorithm: "HS256",
+// 			expiresIn: "30 minutes",
+// 			fields: ["id", "email"], // TODO: this currently does nothing
+// 		},
+// 		refresh: {
+// 			name: "refresh",
+// 			secretKey: "jldmff",
+// 			algorithm: "HS256",
+// 			expiresIn: "30 days",
+// 			fields: ["id"],
+// 		},
+// 	},
+// 	adapter: DrizzleAdapter(db),
+// });
