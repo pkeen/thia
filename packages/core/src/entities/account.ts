509e8d9c554f82ef2ae9abcd8f644b31a58dc29f
pkeen <pkeen7@gmail.com>
2025-09-24 18:03:03 +0100

    reading about domain modelling - some deletions

--- packages/core/src/entities/account.ts
+++ packages/core/src/entities/account.ts
@@ -37,26 +37,4 @@ export interface Account {
 	session_state?: string;
 }
 
-// export const account = z.object({
-// 	userId: z.string(),
-// 	type: accountProviderType,
-// 	/**
-// 	 * This value depends on the type of the provider being used to create the account.
-// 	 * - oauth/oidc: The OAuth account's id, returned from the `profile()` callback.
-// 	 * - email: The user's email address.
-// 	 * - credentials: `id` returned from the `authorize()` callback
-// 	 */
-// 	providerAccountId: z.string(),
-// 	/** Provider's id for this account. - should be an enum - TODO -- E.g. "google". See the full list at https://authjs.dev/reference/core/providers */
-// 	provider: z.string(),
-// 	access_token: z.string().optional(),
-// 	expires_at: z.number().optional(),
-// 	token_type: z.string().optional(),
-// 	scope: z.string().optional(),
-// 	refresh_token: z.string().optional(),
-// 	id_token: z.string().optional(),
-// 	session_state: z.string().optional(),
-// });
-// export type Account = z.infer<typeof account>
-
-export type ProviderType = "oidc" | "oauth" | "email" | "credentials";
+// export type ProviderType = "oidc" | "oauth" | "email" | "credentials";
