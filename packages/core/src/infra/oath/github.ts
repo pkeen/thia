72fac41b48a1a37b19dd5b1a727fee9f4a7e7b25
pkeen <pkeen7@gmail.com>
2026-02-12 19:26:14 +0000

    begin building a nextjs auth thia

--- packages/core/src/infra/oath/github.ts
+++ packages/core/src/infra/oath/github.ts
@@ -66,6 +66,80 @@ const GitHubProfileSchema = z.object({
 type GitHubUserProfile = z.infer<typeof GitHubProfileSchema>;
 type GitHubTokens = z.infer<typeof GitHubTokensSchema>;
 
+// export class GitHub
+// 	extends AbstractOAuthProvider<ScopeType, GitHubTokens, GitHubUserProfile>
+// 	implements OAuthProviderPort
+// {
+// 	readonly key = "github";
+// 	readonly name = "GitHub";
+
+// 	private apiBaseUrl = "https://api.github.com";
+
+// 	protected authorizeEndpoint = "https://github.com/login/oauth/authorize";
+// 	protected tokenEndpoint = "https://github.com/login/oauth/access_token";
+
+// 	protected scopeMap = {
+// 		repo: "repo",
+// 		repo_status: "repo:status",
+// 		repo_deployment: "repo_deployment",
+// 		public_repo: "public_repo",
+// 	};
+// 	protected defaultScopes = [];
+
+// 	protected tokenSchema = GitHubTokensSchema;
+// 	protected profileSchema = GitHubProfileSchema;
+
+// 	readonly style = { text: "#fff", bg: "#24292f" };
+
+// 	constructor(config: OAuthProviderConfig) {
+// 		super(config);
+// 	}
+
+// 	async exchangeCodeForTokens(code: string): Promise<GitHubTokens> {
+// 		const tokenUrl = new URL(this.tokenEndpoint);
+// 		tokenUrl.searchParams.set("client_id", this.clientId);
+// 		tokenUrl.searchParams.set("client_secret", this.clientSecret);
+// 		tokenUrl.searchParams.set("redirect_uri", this.redirectUri);
+// 		tokenUrl.searchParams.set("grant_type", "authorization_code");
+// 		tokenUrl.searchParams.set("code", code);
+// 		const headers = new Headers();
+// 		headers.append("Accept", "application/json");
+// 		const data = await fetch(tokenUrl.toString(), {
+// 			method: "POST",
+// 			headers,
+// 		});
+
+// 		const res = await data.json();
+// 		console.log("GITHUB TOKENS:", res);
+// 		return res;
+// 	}
+
+// 	protected convertToUserAccountProfile(
+// 		userProfile: GitHubUserProfile
+// 	): UserAccountProfile {
+// 		const userAccountProfile: UserAccountProfile = {
+// 			accountId: userProfile.id.toString(),
+// 			name: userProfile.name ?? userProfile.login,
+// 			email: userProfile.email,
+// 			image: userProfile.avatar_url,
+// 		};
+// 		console.log("userAccountProfile:", userAccountProfile);
+// 		return userAccountProfile;
+// 	}
+
+// 	protected async fetchPublicProfile(
+// 		accessToken: string
+// 	): Promise<GitHubUserProfile> {
+// 		const url = new URL(`${this.apiBaseUrl}/user`);
+// 		const headers = new Headers();
+// 		headers.append("Authorization", `Bearer ${accessToken}`);
+// 		const response = await fetch(url.toString(), {
+// 			headers,
+// 		});
+// 		return await response.json();
+// 	}
+// }
+
 export class GitHub
 	extends AbstractOAuthProvider<ScopeType, GitHubTokens, GitHubUserProfile>
 	implements OAuthProviderPort
@@ -115,7 +189,7 @@ export class GitHub
 	}
 
 	protected convertToUserAccountProfile(
-		userProfile: GitHubUserProfile
+		userProfile: GitHubUserProfile,
 	): UserAccountProfile {
 		const userAccountProfile: UserAccountProfile = {
 			accountId: userProfile.id.toString(),
@@ -128,7 +202,7 @@ export class GitHub
 	}
 
 	protected async fetchPublicProfile(
-		accessToken: string
+		accessToken: string,
 	): Promise<GitHubUserProfile> {
 		const url = new URL(`${this.apiBaseUrl}/user`);
 		const headers = new Headers();
@@ -139,6 +213,7 @@ export class GitHub
 		return await response.json();
 	}
 }
+
 
 // export class GitHub extends AbstractOAuthProvider<ScopeType> {
 // 	readonly type = "oauth";
